4ea8e582c48a704e56714834a6045ef261019ab8
Akshay E <akshaykumar2002817@gmail.com>
2024-04-01 15:44:53 +0530

    changed to index.js

--- index.js
+++ index.js
@@ -66,13 +66,13 @@ app.use('/products', productRoute);
 
 async function connectToMongoDB() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/project1');
+    await mongoose.connect(process.env.MONGODB_URI ); 
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
   }
 }
 
-connectToMongoDB();
+connectToMongoDB(); 
 
 app.listen(port, () => console.log(`Listening to the server on http://localhost:${port}/admin`));
